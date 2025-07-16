f3085224307a8ff5040427fbbcf327cf3de1ac31
Christopher Long <chris@thesoftwaresage.com>
2025-07-15 20:05:36 -0400

    Updating header

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -8,8 +8,8 @@ module.exports = {
     'text-white',
     'text-gray-800',
     'text-gray-700',
-    'text-blue-600',
-    'hover:text-blue-300'
+    'hover:text-blue-300',
+    'prose'
   ],
   theme: {
     extend: {}
