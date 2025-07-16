756eb147006a3318d2a46068fba467e389ef68b4
Christopher Long <chris@thesoftwaresage.com>
2025-07-15 21:25:28 -0400

    Updating header

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,11 +5,18 @@ const typography = require('@tailwindcss/typography');
 module.exports = {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   safelist: [
-    'text-white',
-    'text-gray-800',
-    'text-gray-700',
-    'hover:text-blue-300',
-    'prose'
+    {
+      pattern: /text-(white|gray-\d{3}|red-\d{3}|blue-\d{3})/
+    },
+    {
+      pattern: /bg-(white|gray-\d{3}|red-\d{3}|blue-\d{3})/
+    },
+    {
+      pattern: /hover:text-(blue-\d{3}|white)/
+    },
+    {
+      pattern: /(prose|font-bold|shadow-md|sticky|top-\d+|z-\d+)/
+    }
   ],
   theme: {
     extend: {}
