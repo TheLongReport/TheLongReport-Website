a79bdd8acae083b2ed0169c7cad55673cf2f0b5a
Christopher Long <chris@thesoftwarewsage.com>
2025-07-22 16:48:09 -0400

    Trying to fix the issue with the headers

--- svelte.config.js
+++ svelte.config.js
@@ -10,12 +10,7 @@ const config = {
       pages: 'build',
       assets: 'build',
       fallback: 'index.html', // SPA fallback for Azure
-      precompress: false,
-      strict: true
     }),
-    alias: {
-      $lib: 'src/lib'
-    }
   }
 };
 
