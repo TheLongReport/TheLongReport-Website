7d80683ebea7ca9ba26fb8f1b3b0ce79acae2639
Christopher Long <chris@thesoftwarewsage.com>
2025-07-22 16:57:13 -0400

    Trying to fix the issue with the headers

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,17 +3,16 @@
 </script>
 
 
-<!-- Temporarily replace header with div -->
-<div class="bg-red-500 shadow-md sticky top-0 z-50">
+<header class="bg-red-500 shadow-md sticky inset-x-0 top-[1px] z-50">
   <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-4 flex justify-between items-center">
-    <h1 class="text-white antialiased">The Long Report</h1>
+    <h1  class="text-white antialiased" style="color: white">The Long Report</h1>
     <nav class="space-x-4 text-white font-medium">
       <a href="/" class="hover:text-blue-300">Home</a>
       <a href="/michigan" class="hover:text-blue-300">Michigan</a>
       <a href="/national" class="hover:text-blue-300">National</a>
     </nav>
   </div>
-</div>
+</header>
 
 <main class="max-w-4xl mx-auto px-4 py-8">
   <slot />
