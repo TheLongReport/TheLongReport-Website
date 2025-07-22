2669c0ecb52395bae010d06c18994cdb03e3435a
Christopher Long <chris@thesoftwaresage.com>
2025-07-22 14:28:40 -0400

    Updating header

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,8 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+	build: {
+		minify: false
+	},
 	plugins: [tailwindcss(), sveltekit()]
 });
