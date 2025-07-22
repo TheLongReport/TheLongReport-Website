e48acc74cd9aa608d7d57c421f736089490e059c
Christopher Long <chris@thesoftwarewsage.com>
2025-07-22 16:41:56 -0400

    Trying to fix the issue with the headers

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,10 +4,8 @@
 
 
 <header class="bg-red-500 shadow-md sticky top-0 z-50">
-  <h1 class="text-black antialiased">The Long Report</h1>
-
   <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-4 flex justify-between items-center">
-    <h1 class="text-green antialiased">The Long Report</h1>
+    <h1 class="text-white antialiased">The Long Report</h1>
     <nav class="space-x-4 text-white font-medium">
       <a href="/" class="hover:text-blue-300">Home</a>
       <a href="/michigan" class="hover:text-blue-300">Michigan</a>
